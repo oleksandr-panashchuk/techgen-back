b3864328df264bf4b236d16c5af4b33b160da0af
Monolit2000 <kostuchenkojr@gmail.com>
2023-04-12 15:11:43 +0300

    Controller update

--- Techgen/Controllers/v1/VacanciesParserController.cs
+++ Techgen/Controllers/v1/VacanciesParserController.cs
@@ -5,6 +5,8 @@ using Techgen.Models.ResponseModels;
 using Techgen.Services.Interfaces;
 using Microsoft.Extensions.Localization;
 using Techgen.ResourceLibrary;
+using Techgen.Common.Constants;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Techgen.Controllers.v1
 {
@@ -23,6 +25,11 @@ namespace Techgen.Controllers.v1
         
         [Route("get-vacancies")]
         [HttpGet]
+        [SwaggerResponse(200, ResponseMessages.RequestSuccessful, typeof(JsonResponse<IBaseResponse<VacancyResponseModel>>))]
+        [SwaggerResponse(400, ResponseMessages.InvalidData, typeof(ErrorResponseModel))]
+        [SwaggerResponse(401, ResponseMessages.Unauthorized, typeof(ErrorResponseModel))]
+        [SwaggerResponse(403, ResponseMessages.Forbidden, typeof(ErrorResponseModel))]
+        [SwaggerResponse(500, ResponseMessages.InternalServerError, typeof(ErrorResponseModel))]
         public async Task<IActionResult> Get([FromQuery] VacancyRequestModel model)
         {
             var response = await _vacanciesParser.GetAll(model);
